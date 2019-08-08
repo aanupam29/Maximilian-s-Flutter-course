65e5194d5ceb9de4c9c475c83fd983336f61594b
AlecDr <alecsander_rs@hotmail.com>
2019-08-08 19:01:10 -0300

    Conditional rendering of question

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MainPage());
+void main() => runApp(MainPage());
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
 }
 
-class MainPage extends StatelessWidget {
+class _MainPageState extends State<MainPage> {
+  int questionIndex = 0;
+
+  void _onAnswerSelected(BuildContext context) {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite drink?',
+    ];
+
+    Widget _renderText() {
+      return questionIndex < questions.length
+          ? Text(questions[questionIndex])
+          : Text('Question not found!');
+    }
+
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: _renderText(),
+            ),
+            RaisedButton(
+              child: Text('Change question!'),
+              onPressed: () => _onAnswerSelected(context),
+            )
+          ],
         ),
         appBar: AppBar(
           title: Text('Quiz App'),

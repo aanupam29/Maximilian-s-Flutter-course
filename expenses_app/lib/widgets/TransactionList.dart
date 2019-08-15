51bea0460becb4e8b3c4d4f266a46a97cb6e17d0
AlecDr <alecsander_rs@hotmail.com>
2019-08-14 21:50:18 -0300

    Adding a scrollable list

--- expenses_app/lib/widgets/TransactionList.dart
+++ expenses_app/lib/widgets/TransactionList.dart
@@ -9,30 +9,40 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ...transactions.map((transaction) {
-          return Card(
-            child: ListTile(
-              leading: PriceTag(transaction.amount),
-              title: Text(
-                transaction.description,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+    return Expanded(
+      child: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Your Transactions',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            alignment: Alignment(0, 0),
+          ),
+          ...transactions.map((transaction) {
+            return Card(
+              child: ListTile(
+                leading: PriceTag(transaction.amount),
+                title: Text(
+                  transaction.description,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  transaction.getDate(),
+                  style: TextStyle(color: Colors.grey),
+                ),
+                trailing: Icon(
+                  Icons.remove_circle_outline,
+                  color: Colors.red,
                 ),
               ),
-              subtitle: Text(
-                transaction.getDate(),
-                style: TextStyle(color: Colors.grey),
-              ),
-              trailing: Icon(
-                Icons.remove_circle_outline,
-                color: Colors.red,
-              ),
-            ),
-          );
-        }).toList()
-      ],
+            );
+          }).toList()
+        ],
+      ),
     );
   }
 }
--- expenses_app/lib/widgets/Transactions.dart
+++ expenses_app/lib/widgets/Transactions.dart
@@ -25,7 +25,7 @@ class _TransactionsState extends State<Transactions> {
     ),
   ];
 
-  void addTransaction(Transaction transaction) {
+  void _addTransaction(Transaction transaction) {
     setState(() {
       transactions.add(transaction);
     });
@@ -37,7 +37,7 @@ class _TransactionsState extends State<Transactions> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Chart(),
-        TransactionForm(addTransaction: addTransaction),
+        TransactionForm(addTransaction: _addTransaction),
         TransactionList(
           transactions: transactions,
         ),

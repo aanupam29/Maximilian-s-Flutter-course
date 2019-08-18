bb1ba6f9ad5f4b4b15bda2d626159c4f6ad8e2a3
AlecDr <alecsander_rs@hotmail.com>
2019-08-18 11:01:59 -0300

    Chaging the app font to Quicksand

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
+        fontFamily: 'Quicksand',
       ),
       home: MyHomePage(),
     );

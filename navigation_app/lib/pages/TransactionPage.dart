c314915cc65a4a53fda4ae5980d46d832f9492d7
AlecDr <alecsander_rs@hotmail.com>
2019-08-28 22:23:26 -0300

    returning a promise on native back  press

--- navigation_app/lib/pages/TransactionPage.dart
+++ navigation_app/lib/pages/TransactionPage.dart
@@ -8,65 +8,71 @@ class TransactionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "\$ ${selectedTransaction.getAmount()}",
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
+    return WillPopScope(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "\$ ${selectedTransaction.getAmount()}",
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                selectedTransaction.description,
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
+                Text(
+                  selectedTransaction.description,
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                selectedTransaction.getDate(),
-                style: TextStyle(
-                  fontSize: 15,
+                Text(
+                  selectedTransaction.getDate(),
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    MaterialButton(
-                      child: Text('< Go back'),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      onPressed: () {
-                        Navigator.pop(context, false);
-                      },
-                    ),
-                    MaterialButton(
-                      child: Text('Remove!'),
-                      textColor: Colors.white,
-                      color: Colors.red,
-                      onPressed: () {
-                        Navigator.pop(context, true);
-                      },
-                    ),
-                  ],
-                ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      MaterialButton(
+                        child: Text('< Go back'),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        onPressed: () {
+                          Navigator.pop(context, false);
+                        },
+                      ),
+                      MaterialButton(
+                        child: Text('Remove!'),
+                        textColor: Colors.white,
+                        color: Colors.red,
+                        onPressed: () {
+                          Navigator.pop(context, true);
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
+        appBar: AppBar(
+          title: Text('Transaction'),
+        ),
       ),
-      appBar: AppBar(
-        title: Text('Transaction'),
-      ),
+      onWillPop: () async {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
     );
   }
 }

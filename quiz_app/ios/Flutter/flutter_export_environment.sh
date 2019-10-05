19fe8b135390c7618d5948999484da7cdec9d3ff
AlecDr <alecsander_rs@hotmail.com>
2019-10-05 16:24:37 -0300

    updating pubspec

--- quiz_app/ios/Flutter/flutter_export_environment.sh
+++ quiz_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=F:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\code\Maximilian-s-Flutter-course\meals_app"
+export "FLUTTER_APPLICATION_PATH=C:\code\Maximilian-s-Flutter-course\quiz_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"

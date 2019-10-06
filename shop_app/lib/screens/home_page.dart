cc5afad1f320ebee893544ee4725739d2c2dadc2
AlecDr <alecsander_rs@hotmail.com>
2019-10-06 17:53:31 -0300

    basic structure added

---
+++ shop_app/lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop App'),
+      ),
+      body: Container(
+        child: Text('Shop app'),
+      ),
+    );
+  }
+}

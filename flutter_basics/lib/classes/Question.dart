6311da2f1eb1a25b6a50b666016615494e01c9e9
AlecDr <alecsander_rs@hotmail.com>
2020-01-12 16:42:26 -0300

    display questions and answers

---
+++ flutter_basics/lib/classes/Question.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Question {
+  String question;
+  List<String> answers;
+
+  Question(this.question, this.answers) {}
+}
--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,47 +1,84 @@
+// flutter
 import 'package:flutter/material.dart';
+
+// custom
+import 'package:flutter_basics/classes/Question.dart';
 
 void main() => runApp(BasicsApp());
 
-class BasicsApp extends StatelessWidget {
-  void onPressAnswerButton({String answer = 'answer'}) {
+class BasicsApp extends StatefulWidget {
+  @override
+  _BasicsAppState createState() => _BasicsAppState();
+}
+
+class _BasicsAppState extends State<BasicsApp> {
+  List<Question> questions = [
+    Question('What\'s your favorite color?', ['Blue', 'Red', 'Green']),
+    Question('What\'s your favorite meal?', ['Pizza', 'Meat', 'Hamburger']),
+    Question('What\'s your favorite animal?', ['Dog', 'Cat', 'Horse'])
+  ];
+
+  int questionIndex = 0;
+
+  List<String> answers = [];
+
+  void _onPressAnswerButton({String answer}) {
+    setState(() {
+      questionIndex++;
+      this.answers.add(answer);
+    });
     print(answer);
+  }
+
+  String _getHeaderText() {
+    print(this.questionIndex);
+    return questionIndex < this.questions.length
+        ? this.questions[this.questionIndex].question
+        : 'Your Answers';
+  }
+
+  Widget _getAnswersOrAnswered() {
+    return questionIndex < this.questions.length
+        ? Column(
+            children: this
+                .questions[questionIndex]
+                .answers
+                .map(
+                  (answer) => RaisedButton(
+                    child: Text(answer),
+                    onPressed: () {
+                      _onPressAnswerButton(answer: answer);
+                    },
+                  ),
+                )
+                .toList(),
+          )
+        : Column(
+            children: this.answers.map((answer) => Text(answer)).toList(),
+          );
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite meal?',
-      'What\'s your favorite animal?'
-    ];
-
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Basics'),
         ),
-        body: Container(
+        body: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Container(
+            width: double.infinity,
             child: Column(
-          children: <Widget>[
-            Text('The question'),
-            RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: () {
-                  onPressAnswerButton(answer: 'Answer 1');
-                }),
-            RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: () {
-                  onPressAnswerButton(answer: 'Answer 2');
-                }),
-            RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: () {
-                  onPressAnswerButton(answer: 'Answer 3');
-                }),
-          ],
-        )),
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(this._getHeaderText()),
+                this._getAnswersOrAnswered()
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }

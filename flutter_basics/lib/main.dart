0ecdef87261de65ad007dfb6c131d16f236214bc
AlecDr <alecsander_rs@hotmail.com>
2020-01-12 15:06:22 -0300

    add a simple scaffold with an AppBar and a centered Text

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(BasicsApp());
+}
+
+class BasicsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Basics'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('The flutter basics app'),
+          ),
+        ),
+      ),
+    );
+  }
+}

4534485a2114256730bf2b025aacfb3aaa9a7979
AlecDr <alecsander_rs@hotmail.com>
2020-01-12 16:02:17 -0300

    add a title and buttons with actions

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(BasicsApp());
-}
+void main() => runApp(BasicsApp());
 
 class BasicsApp extends StatelessWidget {
+  void onPressAnswerButton({String answer = 'answer'}) {
+    print(answer);
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite meal?',
+      'What\'s your favorite animal?'
+    ];
+
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
@@ -14,10 +22,26 @@ class BasicsApp extends StatelessWidget {
           title: Text('Flutter Basics'),
         ),
         body: Container(
-          child: Center(
-            child: Text('The flutter basics app'),
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            Text('The question'),
+            RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: () {
+                  onPressAnswerButton(answer: 'Answer 1');
+                }),
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () {
+                  onPressAnswerButton(answer: 'Answer 2');
+                }),
+            RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  onPressAnswerButton(answer: 'Answer 3');
+                }),
+          ],
+        )),
       ),
     );
   }

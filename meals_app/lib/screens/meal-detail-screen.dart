423d50189e8e5c56911bad432799244167cfe97e
AlecDr <alecsander_rs@hotmail.com>
2020-01-19 13:41:42 -0300

    display steps

--- meals_app/lib/screens/meal-detail-screen.dart
+++ meals_app/lib/screens/meal-detail-screen.dart
@@ -6,52 +6,95 @@ class MealDetailScreen extends StatelessWidget {
 
   MealDetailScreen(this.meal);
 
+  Widget _buildContainter(Widget child) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        height: 200,
+        width: 250,
+        child: child);
+  }
+
+  List<Widget> _buildIngredients(BuildContext context) {
+    return [
+      Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Text(
+          'Ingredients',
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+      this._buildContainter(
+        ListView.builder(
+            itemBuilder: (BuildContext _, int index) {
+              return Card(
+                color: Theme.of(context).accentColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(this.meal.ingredients[index]),
+                ),
+              );
+            },
+            itemCount: this.meal.ingredients.length),
+      )
+    ];
+  }
+
+  List<Widget> buildSteps(BuildContext context) {
+    return [
+      Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Text(
+          'Steps',
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+      this._buildContainter(
+        ListView.builder(
+            itemBuilder: (BuildContext _, int index) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text("#${index + 1}"),
+                  ),
+                  title: Text(this.meal.steps[index]),
+                ),
+              );
+            },
+            itemCount: this.meal.steps.length),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(this.meal.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              this.meal.imageUrl,
-              fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(bottom: 20),
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                this.meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.grey,
-                ),
-                borderRadius: BorderRadius.circular(10)),
-            height: 200,
-            width: 250,
-            child: ListView.builder(
-                itemBuilder: (BuildContext _, int index) {
-                  return Card(
-                    color: Theme.of(context).accentColor,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(this.meal.ingredients[index]),
-                    ),
-                  );
-                },
-                itemCount: this.meal.ingredients.length),
-          )
-        ],
+            ...this._buildIngredients(context),
+            ...this.buildSteps(context),
+          ],
+        ),
       ),
     );
   }

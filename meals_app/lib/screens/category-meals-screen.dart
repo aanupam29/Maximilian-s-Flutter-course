1ccb934b343a09af8e0c086e7fa3033f48718921
AlecDr <alecsander_rs@hotmail.com>
2020-01-19 12:38:40 -0300

    display meal item

--- meals_app/lib/screens/category-meals-screen.dart
+++ meals_app/lib/screens/category-meals-screen.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/Category.dart';
+import 'package:meals_app/models/Meal.dart';
+import 'package:meals_app/widgets/meal-item.dart';
+import '../data/dummy-data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   final Category category;
+  List<Meal> meals;
 
-  CategoryMealsScreen(this.category);
+  CategoryMealsScreen(this.category) {
+    this.meals = DUMMY_MEALS.where((Meal meal) {
+      return meal.categoriesIds.contains(this.category.id);
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(this.category.title),
-      ),
-      body: Center(
-        child: Text('Recipes'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(this.category.title),
+        ),
+        body: ListView.builder(
+          itemBuilder: (BuildContext _, int index) {
+            return MealItem(this.meals[index]);
+          },
+          itemCount: this.meals.length,
+        ));
   }
 }
---
+++ meals_app/lib/widgets/meal-item.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/Meal.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  MealItem(this.meal);
+
+  void onSelectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onSelectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    this.meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 30,
+                  left: 20,
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black45,
+                    ),
+                    width: 240,
+                    child: Text(
+                      this.meal.title,
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(this.meal.duration.toString() + 'min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(this.meal.complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(this.meal.affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
